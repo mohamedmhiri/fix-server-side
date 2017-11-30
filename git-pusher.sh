0ac27394c562e9588f5d32c857fee2e263ea6fef
mohamed mhiri <mohamed.mohamed.mhiri@gmail.com>
2017-11-30 12:34:42 +0100

    rotation-file-stream package resolved

--- git-pusher.sh
+++ git-pusher.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+#cp -r ../stock-exchange-server-side/node_modules/rotating-file-stream/ ./node_modules/
+
 git add -A; git commit -m "$*"; git push github master; git push gitlab master
