e69c1bb47eca705b8e281186f121bb8fb63d9241
mohamed <med.mhiri.5@gmail.com>
2017-12-16 10:22:43 +0100

    init

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use('/api', transaction)
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '5000'
+const port = process.env.PORT || '4000'
 app.set('port', port)
 
 /**
